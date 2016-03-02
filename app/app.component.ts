7d7f517bdcf0aebf14d62ddf5a7ce95c6a37cc42
Farhan Chauhan <farhan687@gmail.com>
2016-03-02 19:47:08 +0530

    Get/Set config from localstorage

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {HeaderComponent} from "./header/header.component";
 import {BuildComponent} from "./build/build.component";
 import {ResultComponent} from "./result/result.component";
@@ -12,23 +12,51 @@ import {$http} from './shared/httpwrap'
 	directives: [BuildComponent, ResultComponent, RunComponent]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 	public heros: string = 'asdjfkse';
 	public mapping: any = { 
 							types: [], 
+							mapping: null,
 							resultQuery:{'type':'',
 								'result':[],
 								'final':"{}"
 							},
 							output:{}
 						};
+	
+	//For default config
+	// public config: Config = {
+	// 	url: "https://uHg3p7p70:155898a9-e597-430e-8e2b-61fd1914c0d0@scalr.api.appbase.io",
+	// 	appname: "moviedb",
+	// 	username: "",
+ 	// 		password: "" 
+	// };
+
 	public config: Config = {
-		url: "https://uHg3p7p70:155898a9-e597-430e-8e2b-61fd1914c0d0@scalr.api.appbase.io",
-			appname: "moviedb",
-			username: "",
-	 		password: "" 
+		url: "",
+		appname: "",
+		username: "",
+		password: "" 
 	};
 
+	ngOnInit() {
+		this.getLocalConfig();
+  	}
+
+	getLocalConfig() {
+		var url = window.localStorage.getItem('url');
+		var appname = window.localStorage.getItem('appname');
+		if(url != null) {
+			this.config.url = url;
+			this.config.appname = appname;
+			this.connect();
+		}
+	}
+
+	setLocalConfig(url, appname) {
+		window.localStorage.setItem('url', url);
+		window.localStorage.setItem('appname', appname);
+	}
 
 	connect() {
 		var APPNAME = this.config.appname;
@@ -40,10 +68,10 @@ export class AppComponent {
 		var self = this;
 		var createUrl = this.config.url + '/' + this.config.appname + '/_mapping';
 		var autho = "Basic " + btoa(self.config.username + ':' + self.config.password);
-		console.log(autho);
 		$http.get(createUrl, autho).then(function(res) {
 			self.mapping.mapping = res;
 			self.mapping.types = self.seprateType(res);
+			self.setLocalConfig(self.config.url, self.config.appname);
 	   	});
 	}
 		
