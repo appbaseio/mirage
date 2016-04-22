304258fbdaee36aa3705e98a03c881ef6d58d120
Farhan Chauhan <farhan687@gmail.com>
2016-04-22 13:04:49 +0530

    Add Query response in popup

--- app/result/result.component.ts
+++ app/result/result.component.ts
@@ -16,6 +16,7 @@ export class ResultComponent implements OnInit {
 	public mapping;
 	public config;
 	public editorHookHelp;
+	public responseHookHelp;
 
 	constructor(private mappingService: MappingService) { }
 	
@@ -24,7 +25,7 @@ export class ResultComponent implements OnInit {
 		this.editorHookHelp.applyEditor();
 		var resultHeight = $(window).height() - 170;
     	$('.queryRight .codemirror').css({  height:resultHeight });
-	}
+    }
 
 
 	runQuery() {
@@ -32,11 +33,21 @@ export class ResultComponent implements OnInit {
 		var createUrl = this.config.url + '/' + this.config.appname + '/'+ this.mapping.selectedTypes + '/_search';
 		var autho = "Basic " + btoa(self.config.username + ':' + self.config.password);
 		var getQuery = this.editorHookHelp.getValue();
-		var payload = JSON.parse(getQuery);
-		// console.log(this.mapping.resultQuery);
-		$http.post(createUrl, payload, autho).then(function(res) {
-			self.mapping.output = JSON.stringify(res, null, 2);
-			self.responseHookHelp.setValue(self.mapping.output);
-		});
+		var payload = null;
+		try {
+			payload = JSON.parse(getQuery);
+		} catch(e) {
+			alert('Json is not valid');
+		}
+		if(payload) {
+			// self.mapping.isWatching = true;
+			self.responseHookHelp.setValue('{"Loading": "please wait......"}');
+			$('#resultModal').modal('show');
+			$http.post(createUrl, payload, autho).then(function(res) {
+				// self.mapping.isWatching = false;
+				self.mapping.output = JSON.stringify(res, null, 2);
+				self.responseHookHelp.setValue(self.mapping.output);
+			});
+		}
 	}
 }
