b8616cd71811b8177ff7284c5ac3a12cdafafbb5
Farhan Chauhan <farhan687@gmail.com>
2016-06-03 16:41:04 +0530

    Add popover on query field

--- app/build/select2/select2.component.ts
+++ app/build/select2/select2.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnChanges, SimpleChange, Input, Output, AfterContentInit, EventEmitter } from "@angular/core";
+import { queryList } from "../../shared/queryList";
 
 @Component({
 	selector: 'select2',
 	templateUrl: './app/build/select2/select2.component.html',
-	inputs: ["selectModal", "selectOptions", "querySelector", "selector"]
+	inputs: ["selectModal", "selectOptions", "querySelector", "selector", "showInfoFlag"]
 })
 
 export class select2Component implements OnChanges, AfterContentInit {
 	@Input() querySelector;
 	@Input() selector;
+	@Input() showInfoFlag;
 	@Output() callback = new EventEmitter();
 	public select2Selector;
+	public queryList = queryList;
 
 	constructor() {}
 
@@ -37,5 +40,24 @@ export class select2Component implements OnChanges, AfterContentInit {
 		field_select.on("change", function(e) {
 			callback(field_select.val())
 		}.bind(this));
+		if(this.showInfoFlag) {
+			field_select.on("select2:open", function(e) {
+				setTimeout(function() {
+					var selector = $('li.select2-results__option');
+					selector.each(function(i, item) {
+						var val = $(item).html();
+						var info = this.getInformation(val);
+						$(item).popover(info);
+					}.bind(this))
+				}.bind(this), 300);
+			}.bind(this));
+		}
+	}
+
+	getInformation(query) {
+		console.log(this.queryList);
+		var query = this.queryList['information'][query];
+		query['trigger'] = 'hover';
+		return query;
 	}
 }
