70bb4305d9f46b8009d2df38d2c7e3fccd49683e
Farhan Chauhan <farhan687@gmail.com>
2016-06-13 12:49:38 +0530

    Show replative time with saved query

--- app/shared/pipes/prettyJson.ts
+++ app/shared/pipes/prettyJson.ts
@@ -1,23 +1,8 @@
 import {Pipe, PipeTransform} from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
-declare var moment;
+
 @Pipe({name: 'prettyJson'})
 export class prettyJson implements PipeTransform {
   transform(value : Object) : any {
 	return JSON.stringify(value, null, 4);
-  }
-}
-@Pipe({name: 'prettyTime'})
-export class prettyTime implements PipeTransform {
-  transform(value : Object) : any {
-	return moment(value, 'DD-MM hh:mm A');
   }
 }
--- app/shared/pipes/prettyTime.ts
+++ app/shared/pipes/prettyTime.ts
@@ -1,17 +1,9 @@
 import {Pipe, PipeTransform} from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
 declare var moment;
+
 @Pipe({name: 'prettyTime'})
 export class prettyTime implements PipeTransform {
   transform(value : Object) : any {
-  	return moment(value).format('DD-MM hh:mm A');
+  	return moment(value).fromNow();
   }
 }
