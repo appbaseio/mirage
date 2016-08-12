29aa9eea4627ae48cfa4aef611dc7371a6dc9fac
Farhan Chauhan <farhan687@gmail.com>
2016-08-12 13:01:34 +0530

    Remove unused folder

--- mirage-unpacked/site/app/shared/pipes/prettyJson.ts
+++
@@ -1,8 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-
-@Pipe({name: 'prettyJson'})
-export class prettyJson implements PipeTransform {
-  transform(value : Object) : any {
-	return JSON.stringify(value, null, 4);
-  }
-}
