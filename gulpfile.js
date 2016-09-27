13a7e0bfe0028b62e08cfa7d8271640e45a120b2
farhan <farhan687@gmail.com>
2016-09-27 12:43:55 +0530

    Add polyfill shim js for old browser

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ var files = {
     },
     js: {
         vendor: [
+            'node_modules/core-js/client/shim.min.js',
             'node_modules/zone.js/dist/zone.js',
             'node_modules/reflect-metadata/Reflect.js',
             'node_modules/systemjs/dist/system.src.js',
