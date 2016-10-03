8ee3eacebb20bc76d90ff3742314a6b9f1dc7068
farhan chauhan <farhan687@gmail.com>
2016-10-03 17:16:04 +0530

    Add codeclimate related files

---
+++ tslint.js
@@ -0,0 +1,24 @@
+{
+  "extends": "tslint:latest",
+  "rules": {
+    "class-name": true,
+    "semicolon": [true, "always"],
+    "triple-equals": [true, "allow-null-check"],
+    "eofline": true,
+    "whitespace": [true,
+      "check-decl",
+      "check-operator",
+      "check-separator",
+      "check-type"
+    ],
+    "variable-name": [
+      "allow-leading-underscore"
+    ],
+    "curly": false,
+    "no-reference": false,
+    "no-var-requires": false,
+    "ordered-imports": false,
+    "no-trailing-whitespace": false
+  },
+  "rulesDirectory": []
+}
