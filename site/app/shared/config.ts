75b76a7dea005952a8b27d31b8e728e51adfa290
farhan chauhan <farhan687@gmail.com>
2016-10-06 20:16:56 +0530

    Remove site folder

--- site/app/shared/config.ts
+++
@@ -1,7 +0,0 @@
-export interface Config {
-	url: string;
-	appname: string;
-	username: string;
-	password: string;
-	host: string;
-}
--- site/popup.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.getSelected(null, function(tab) {
-	chrome.tabs.create({
-    	url: 'site/index.html'
-  	});
-});
