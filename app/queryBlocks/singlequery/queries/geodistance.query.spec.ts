c6a74a8c610eed3c5348982345d655e293eff822
Deepak Grover <deepakgrover333@gmail.com>
2016-10-13 13:51:22 +0530

    add xhr call test for geodistance.query

--- app/queryBlocks/singlequery/queries/geodistance.query.spec.ts
+++ app/queryBlocks/singlequery/queries/geodistance.query.spec.ts
@@ -41,7 +41,7 @@ describe('geo_distance query format', () => {
                 value: '10'
             },
             distance: {
-                value: '200km'
+                value: '100km'
             }
         };
     });
@@ -86,4 +86,73 @@ describe('geo_distance query format', () => {
         var format = query.setFormat();
         expect(format).toEqual(expectedFormatWithOption);
     });
+});
+
+declare var $;
+describe("xhr test (geo_distance)", function () {
+    var returnedJSON: any = {};
+    var status = 0;
+
+    beforeEach(function (done) {
+        var query = new GeoDistanceQuery();
+        query.queryName = 'geo_distance';
+        query.fieldName = 'place';
+        query.inputs = {
+            lat: {
+                value: '10'
+            },
+            lon: {
+                value: '10'
+            },
+            distance: {
+                value: '100km'
+            }
+        };
+        var config = {
+            url: 'https://scalr.api.appbase.io',
+            appname: 'mirage_test',
+            username: 'wvCmyBy3D',
+            password: '7a7078e0-0204-4ccf-9715-c720f24754f2'
+        };
+        var url = 'https://scalr.api.appbase.io/mirage_test/geo/_search';
+        var query_data = query.setFormat();
+        var request_data = {
+            "query": {
+                "bool": {
+                    "must": [query_data]
+                }
+            }
+        };
+        $.ajax({
+            type: 'POST',
+            beforeSend: function(request) {
+                request.setRequestHeader("Authorization", "Basic " + btoa(config.username + ':' + config.password));
+            },
+            url: url,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(request_data),
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(res) {
+                returnedJSON = res;
+                status = 200;
+                done();
+            },
+            error: function(xhr) {
+                returnedJSON = xhr;
+                status = xhr.status;
+                done();
+            }
+        });
+    });
+
+    it("Should have returned JSON and Should have atleast 1 record", function () {
+        expect(returnedJSON).not.toEqual({});
+        expect(returnedJSON).not.toBeUndefined();
+        expect(status).toEqual(200);
+        expect(returnedJSON.hits.hits.length).toBeGreaterThan(0);
+    });
+
 });
