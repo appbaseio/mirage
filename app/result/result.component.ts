31c28a89f2c43f55bf2441ab61bb6e2cde9bcc0d
farhan687 <farhan687@gmail.com>
2017-01-17 14:15:05 +0530

    Set dejavu in result

--- app/result/result.component.ts
+++ app/result/result.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges } from "@angular/core";
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 declare var $: any;
 
 @Component({
 	selector: 'query-result',
 	templateUrl: './app/result/result.component.html',
-	inputs: ['mapping', 'config', 'responseHookHelp', 'result_time_taken', 'types', 'selectedTypes', 'result', 'config', 'responseHookHelp', 'result_time_taken']
+	inputs: ['mapping', 'config', 'editorHookHelp', 'urlShare', 'responseHookHelp', 'result_time_taken', 'types', 'selectedTypes', 'result', 'config', 'responseHookHelp', 'result_time_taken']
 })
 
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, OnChanges {
 	public mapping: any;
 	public config: any;
 	public responseHookHelp: any;
+	public url: SafeResourceUrl;
+	public urlShare: any;
+	public editorHookHelp: any;
+	public urlAvailable: boolean = false;
+	
+	constructor(private sanitizer: DomSanitizer) {}
 
 	ngOnInit() {
 		this.responseHookHelp.applyEditor({ readOnly: true });
 	}
+
+	ngOnChanges(changes) {
+		if(changes && changes['result_time_taken']) {
+			var prev = changes['result_time_taken'].previousValue;
+			var current = changes['result_time_taken'].currentValue;
+			if(current && prev !== current && this.editorHookHelp) {
+				var getQuery = this.editorHookHelp.getValue();
+				if(getQuery) {
+					getQuery = getQuery.trim();
+					getQuery = JSON.stringify(JSON.parse(getQuery));
+					var url = 'http://localhost:1358/#?input_state='+this.urlShare.url;
+					url = url+'&hf=false&sidebar=false&query='+getQuery;
+					this.url = this.sanitizeUrl(url);
+					this.urlAvailable = true;
+				}
+			}
+		}
+	}
+
+	sanitizeUrl(url) {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+	}
+
 }
